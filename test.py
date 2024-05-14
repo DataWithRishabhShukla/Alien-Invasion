04aa65208b54d5bbc2ac3d4893847b1dbc07ce6e
rishabhshukla <rishabh.shukla8@gmail.com>
2024-05-14 14:26:07 +0530

    Added class to manage game assets and behaviour.

--- test.py
+++ test.py
@@ -1,8 +1,15 @@
 import os
 python_ver = os.system('which python')
-print(python_ver)
+print(f'\n\nPython Version :{python_ver}')
 
+"""
+To create and activate python vnev
+python -m venv venv
+source /Users/rishabhshukla/git_projects/Alien-Invasion/alien_invasion_venv/bin/activate
+pip show pygame 
+"""
 
-# To create and activate python vnev
-# python -m venv venv
-# source /Users/rishabhshukla/git_projects/Alien-Invasion/alien_invasion_venv/bin/activate
+import sys
+import pygame
+pygame.init()
+                                      
