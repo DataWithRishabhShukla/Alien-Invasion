debdcd3c37f10b9fa603c28dd814e36f012b3e55
rishabhshukla <rishabh.shukla8@gmail.com>
2024-05-14 13:46:48 +0530

    Updated the gitignore file

--- test.py
+++ test.py
@@ -1,1 +1,8 @@
-print('rishabh')
+import os
+python_ver = os.system('which python')
+print(python_ver)
+
+
+# To create and activate python vnev
+# python -m venv venv
+# source /Users/rishabhshukla/git_projects/Alien-Invasion/alien_invasion_venv/bin/activate
